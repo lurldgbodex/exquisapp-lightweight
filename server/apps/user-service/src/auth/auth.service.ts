5ceb598dc9c84b155d10804cb0768f1b3ea26ed9
Lurldgbodex <professionaleditor6@gmail.com>
2025-05-15 19:10:18 +0100

    chore: publish user created event

--- server/apps/user-service/src/auth/auth.service.ts
+++ server/apps/user-service/src/auth/auth.service.ts
@@ -7,12 +7,14 @@ import { LoginRequest } from '../dto/request/login-user.dto';
 import { User } from '../users/entities/user.entity';
 import { AuthResponse } from '../dto/response/auth-response.dto';
 import { CreateRequest } from '../dto/request/create-user.dto';
+import { RabbitMQService } from 'libs/shared-lib/src';
 
 @Injectable()
 export class AuthService {
     constructor(
         private userService: UsersService,
-        private jwtService: JwtService
+        private jwtService: JwtService,
+        private readonly rabbitMQService: RabbitMQService,
     ) {}
 
     async login(loginDto: LoginRequest): Promise<AuthResponse> {
@@ -21,7 +23,13 @@ export class AuthService {
     }
 
     async register(registerDto: CreateRequest): Promise<AuthResponse> {
+        console.log("Creating User");
         const user = await this.userService.create(registerDto);
+
+        this.publishUserRegisteredEvent(user).catch(err => {
+            console.error('Failed to publish user registered event:', err);
+        });
+                
         return await this.generateToken(user);
     } 
 
@@ -45,5 +53,18 @@ export class AuthService {
         });
 
         return { access_token };
+    }
+
+    private async publishUserRegisteredEvent(user: User) {
+        console.log('Publishing user created event');
+
+        await this.rabbitMQService.publish('user_events', 'user.registered', {
+            eventType: 'USER_REGISTERED',
+            userId: user.id,
+            username: user.username,
+            timeStamp: user.createdAt,
+        });
+
+        console.log('User Registered Event Published');
     }
 }

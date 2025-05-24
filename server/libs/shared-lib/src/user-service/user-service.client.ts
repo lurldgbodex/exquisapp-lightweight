2ab513e1f4f18b4a3071b07b7d07cad413abd92b
Lurldgbodex <professionaleditor6@gmail.com>
2025-05-24 16:31:57 +0100

    chore: config to use env

--- server/libs/shared-lib/src/user-service/user-service.client.ts
+++ server/libs/shared-lib/src/user-service/user-service.client.ts
@@ -2,15 +2,26 @@ import { Injectable } from "@nestjs/common";
 import { HttpService } from '@nestjs/axios';
 import { firstValueFrom } from "rxjs";
 import { UserInfo } from "../events/user.events";
+import { ConfigService } from "@nestjs/config";
 
 @Injectable()
 export class UserServiceClient {
-    constructor(private readonly httpService: HttpService) {}
+    private readonly USER_SERVICE_URL: string | undefined;
+
+    constructor(
+        private readonly httpService: HttpService,
+        private readonly configService: ConfigService
+    ) {
+        this.USER_SERVICE_URL = configService.get<string>('USER_SERVICE_URL');
+        if (!this.USER_SERVICE_URL) {
+            throw new Error('USER_SERVICE_URL is not defined'); 
+        }
+    }
 
     async validateUser(userId: string): Promise<{ isvalid: boolean; userInfo?: UserInfo }> {
         try {
             const response = await firstValueFrom(
-                this.httpService.get(`http://localhost:3001/users/${userId}/validate`),
+                this.httpService.get(`${this.USER_SERVICE_URL}/users/${userId}/validate`),
             );
 
             return { isvalid: true, userInfo: response.data };
